8d25011bdf91d03c8e72ffa52a24ddb3530d65e9
Anton Webb <anton25360@gmail.com>
2020-07-20 20:50:58 +0100

    Created custom component

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <h1>test one: {{test1}}</h1>
+        <h1>test two: {{test2}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    test1: String,
+    test2: String
+  }
+}
+</script>
+
+<style scoped>
+
+h1{
+    color: aqua;
+}
+</style>
